32f921bbca1baa79003bbefe953fd6a1780f1d42
ifs20035-itdel <ifs20035@students.del.ac.id>
2024-01-02 21:09:06 +0700

    day2 : 2 star

--- 2015/day 2/day2.cpp
+++ 2015/day 2/day2.cpp
@@ -23,12 +23,34 @@ int extra_paper(int num1, int num2, int num3){
     }
 }
 
+int tie_bow(int num1, int num2, int num3){
+    if(num1==num2 && num1==num3){
+        return 2*(num1+num2);
+    }
+    int maxNum = max({num1, num2, num3});
+
+    if (maxNum == num1){
+        return 2*(num2+num3);
+    } else if (maxNum == num2){
+        return 2*(num1+num3);
+    } else {
+        return 2*(num1+num2);
+    }
+}
+
 int wrapped(int num1, int num2, int num3){
+    // Return the surface area + extra_paper
     return (2*num1*num2 + 2*num2*num3 + 2*num1*num3) + extra_paper(num1, num2, num3);
 }
 
+int ribbon_bow(int a, int b, int c){
+    // Return the volume + extra to tie the bow
+    return (a*b*c) + tie_bow(a,b,c);
+}
+
 int main(){
-    int result = 0;
+    int paper = 0;
+    int ribbon = 0;
     string filename = "input.txt";
 
     ifstream file(filename);
@@ -39,11 +61,12 @@ int main(){
     while(getline(file, line)){
         stringstream ss(line);
         if (ss>> a >> x >> b >> x >> c){
-            result = result + wrapped(a,b,c);
+            paper = paper + wrapped(a,b,c);
+            ribbon = ribbon + ribbon_bow(a,b,c);
         } else {
             cerr << "Error parsing line: " << line << endl;
         }
     }
 
-    cout << "Result: " << result << endl;
+    cout << "Paper: " << paper << "\nRibbon: " << ribbon << endl;
 }
