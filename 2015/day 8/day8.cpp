3dd168504285f96daedb2bb68255497423716df9
ifs20035-itdel <ifs20035@students.del.ac.id>
2024-01-11 23:48:37 +0700

    day 8 : 1 star

--- 2015/day 8/day8.cpp
+++ 2015/day 8/day8.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
-#include <iomanip>
 
-using namespace std;
+int calculateDifference(const std::string& line) {
+    int codeLength = line.length();
+    int memoryLength = 0;
+    
+    for (size_t i = 1; i < line.length() - 1; ++i) {
+        if (line[i] == '\\' && i + 1 < line.length()) {
+            if (line[i + 1] == '\\' || line[i + 1] == '"') {
+                i++; // Skip next character
+            } else if (line[i + 1] == 'x' && i + 3 < line.length()) {
+                i += 3; // Skip 3 char for hexadec
+            }
+        }
+        memoryLength++;
+    }
+
+    return codeLength - memoryLength;
+}
 
 int main() {
-    ifstream file("input.txt");
-    string line;
-
-    if (file.is_open()) {
-        while (getline(file, line)) {
-            string x = quoted(line);
-            cout << x.length() << " : " << endl;
-            for(char c : x){
-                cout << x << endl;
-            }
-            break;
-        }
-        file.close();
-    } else {
-        cout << "Unable to open file";
+    std::ifstream file("input.txt");
+    if (!file) {
+        std::cerr << "Unable to open file" << std::endl;
         return 1;
     }
+
+    std::string line;
+    int totalDifference = 0;
+
+    while (std::getline(file, line)) {
+        totalDifference += calculateDifference(line);
+    }
+
+    file.close();
+
+    std::cout << "Total difference: " << totalDifference << std::endl;
+
     return 0;
 }
