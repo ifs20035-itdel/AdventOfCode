8ed482a691e176a795951d117f33c0bfb6b05839
ifs20035-itdel <asm34686@gmail.com>
2024-03-01 10:27:09 +0700

    day8 : 2 star

--- 2015/day 8/day8_p2.cpp
+++ 2015/day 8/day8_p2.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+int encodeChar(const string& line) {
+    int codeLength = line.length();
+    int extendedLength = codeLength + 4;
+    for (size_t i = 1; i < line.length() - 1; ++i) {
+        if (line[i] == '\\' && i + 1 < line.length()) {
+            if (line[i + 1] == '\\' || line[i + 1] == '"') {
+                extendedLength += 2;
+                i++; // Skip next character
+            } else if (line[i + 1] == 'x' && i + 3 < line.length()) {
+                extendedLength += 1;
+                i += 3; // Skip 3 char for hexadec
+            } 
+        }
+    }
+    return extendedLength - codeLength;
+}
+
 int main(int argc, char const *argv[])
 {
     ifstream file("input.txt");
@@ -13,13 +30,14 @@ int main(int argc, char const *argv[])
     }
     
     string line;
+    int totalDifference = 0;
 
-    while (getlien(file, line)) {
-        return 0;
+    while (getline(file, line)) {
+        totalDifference += encodeChar(line);
     }
     file.close();
     
-    cout << "result: " << endl;
+    cout << "result: " << totalDifference << endl;
 
     return 0;
 }
